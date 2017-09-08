29c0a974405dd5826d3c0c7b89a7376bda46be39
nyarmith <powah.serge@gmail.com>
2017-09-07 23:19:45 -0400

    Problem bot almost done

--- Telegram_Problem_Bot.py
+++ Telegram_Problem_Bot.py
@@ -8,15 +8,10 @@ import yaml
 import random
 import re
 
-CONFIGFILE   = '/home/reader/Projects/Problem_Notifier/config.yml';
-PROBLEM_DAT  = '/home/reader/Projects/Problem_Notifier/prob_tracking.yml';
+CONFIGFILE   = '/home/reader/Projects/Problem_Notifier_Telegram/config.yml';
+PROBLEM_DAT  = '/home/reader/Projects/Problem_Notifier_Telegram/prob_tracking.yml';
 
-#loaded config file
-CONFIG_SPEC     = None
-prob_tracking   = None
-
-#good sorting
-
+#--good sorting--
 def tryint(s):
     try:
         return int(s)
@@ -34,6 +29,7 @@ def sort_nicely(l):
     """
     l.sort(key=alphanum_key)
 
+
 def get_immediate_subdirectories(a_dir):
     os.chdir('/home/reader/Projects/Problem_Notifier');
     return [name for name in os.listdir(a_dir)
@@ -44,148 +40,151 @@ def removekey(d, key):
     del r[key]
     return r
 
-#emails a random problem, then updates our metadata lib
-def sendRandomProblems(n):
-    global CONFIG_SPEC
-    problem_list = {}
-    #get existing config
-    # params
-    f = open(CONFIGFILE)
-    CONFIG_SPEC = yaml.load(f.read())
-    f.close()
-    # problems to ignore
-    f = open(PROBLEM_DAT)
-    prob_tracking = yaml.load(f.read())
-    f.close()
-
-    #get list of problems in your directories
-    for dir in get_immediate_subdirectories(CONFIG_SPEC['problem_directory']):
-        if not dir.startswith('.'):
-            for subdir in get_immediate_subdirectories(dir):
-                problem_list[dir + '/' + subdir] = set()
-                for file in os.listdir(dir + '/' + subdir):
-                    problem_list[dir + '/' + subdir].add(dir+'/'+subdir+'/'+file)
-    
-    completed_list = set(prob_tracking['sent_problems'])
-    for key in problem_list.keys():
-        problem_list[key] = problem_list[key] - completed_list
-        #remove empty sets
-        if len(problem_list[key]) == 0:
-            problem_list = removekey(problem_list, key)
-
-    to_send = []
-    
-    #pick n random chapters, and pick the first problem from whichever you choose
-    for i in range(n):
-        chapter = random.sample(problem_list.keys(),1)[0]
-        problems = list(problem_list[chapter])
-        sort_nicely(problems)
-        to_send.append(problems[0])
-        problems.pop(0)
-        if len(problems) != 0:
-            problem_list[chapter] = set(problems)
-        else:
-            problem_list = removekey(problem_list[chapter])
-    
-    
-    for i in to_send:
-        sendProblem(i)
-        prob_tracking['sent_problems'].append(i)
-    
-    prob_tracking['sent_problems']
-    f = open(PROBLEM_DAT,'w')
-    f.write(yaml.dump(prob_tracking))
-    f.close()
-
-#formats given problem and sends it to the user
-def sendProblem( prob ):
-    global CONFIG_SPEC
-    #get extension
-    ext = prob.split('.')[-1]
-    message=None
-    f = open(prob,'rb')
-    if ext == 'txt':
-        message = MIMEText(f.read())
-    elif ext == 'png':
-        message = MIMEImage(f.read())
-        message.add_header('Content-ID', '<image1>')
-    f.close()
-    
-    prob_name = prob.split('/')[-1].split('.')[0]
-    sendemail(CONFIG_SPEC['email'], CONFIG_SPEC['recipients'],# [],
-        CONFIG_SPEC['subject'] + ' ' + prob_name, message,
-	CONFIG_SPEC['email'], CONFIG_SPEC['pass'])
- 
-def sendemail(from_addr, to_addr_list,# cc_addr_list,
-              subject, message,
-              login, password,
-              smtpserver='smtp.gmail.com:587'):
-    global CONFIG_SPEC
-    msg = MIMEMultipart()
-    msg['From']      = from_addr
-    msg['To']        = ','.join(to_addr_list)
-    msg['Subject']   = '%s' % subject
-    msg.preamble = 'This is a multi-part message in MIME format.'
-
-    msg.attach(MIMEText('%s <br> <img src="cid:image1"> <br> Enyoy!' % CONFIG_SPEC['msg_header'], 'html'))
-    msg.attach(message)
-
-    server = smtplib.SMTP(smtpserver)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-    server.login(login,password)
-    problems = server.sendmail(from_addr, to_addr_list, msg.as_string())
-    server.quit()
-
-
-#1 problemManager = 1 chat
+#--Problem Manager Definition
+#   1 problemManager = 1 chat
 class ProblemManager(object):
-    def __init__(self, prev_state={}):
-        self.problems = prev_state  #I may just be able to serialize this with json
+    def __init__(self):
+        #dict of "source1" : {"problems_done" : {"Chapter_1" : [] , ..}, "problems_not_done": {"Chapter_1" : [], ..}}, source2: ...
+        self.cur_source = None
+        self.problems   = {}  
+        self.joined_usrs= {}  #user1 : 'ready', user2 : 'ready', user3 : 'not-ready', etc... (but with True and False)
 
     #select pool to draw problems from
     def selectSource(src):
 
     def sendRandomProblem():
 
-    def sendChapterProblem(source, chapter):
+    #def sendChapterProblem(source, chapter): # TODO: as part of mode
 
     def addUser(uname):
+        self.joined_usrs[uname.id] = False
 
     def rmUser(uname):
+        removeKey(self.joined_usrs, uname.id)
 
     #this means the user has understood and is able to move on
     def flagUser(uname):
+        flag = self.joined_usrs[uname]
+        self.joined_usrs[uname]
 
-    #dispenses the next problem based on the dispense-mode
+    #dispenses the next problem (TODO: based on the dispense-mode)
     def dispense():
+        #first fresh problem from random chapter
 
-    def setDispenseMode(uname):
 
-class ProblemBot:
+    #def setDispenseMode(uname):
+
+
+    def all_ready():
+        for i in self.joined_usrs.keys():
+            if self.joined_usrs[i] is False:
+                return False
+
+        return True
+
+
+
+def load(filename):
+   f = open(filename)
+   state = yaml.load(f.read())
+   f.close()
+   return state
+
+def save(obj, filename):
+    f = open(filename, 'w')
+    f.write(yaml.dump(obj))
+    f.close()
+
+
+#Bot Definition
+class ProblemBot(object):
     def __init__(self):
-        if os.path.isfile('chats/listo'):
-            self.load()
+        self.save_file='chats/ProblemBot'
+        if os.path.isfile(self.save):
+            self.chats = load(self.save)
         else:
             self.chats = {}
-    #save current state to file
+
     def save(self):
-        f = open('chats/listo', 'w')
-        f.write(yaml.dump(self.listos))
-        f.close()
+        save(self.chats, self.save_file)
 
     #load file state to object
-    def load(self):
-        f = open('chats/listo')
-        self.chats = yaml.load(f.read())
-        f.close()
+    def load(self, self):
+        self.chats = load(self.save_file)
 
 #these map to new commands
-    def new_chat(self, chat_id):
-        self.chats[chat_id] = ProblemManager()
+    def join(self, chat_id, args):
+        return self.chats[chat_id].addUser(args[0])
 
-    def join(uname):
-        self.chats[chat_id].join(uname)
+    def leave(self, chat_id, args):
+        return self.chats[chat_id].rmUser(args[0])
+
+    def ready(self, chat_id, args):
+        state = self.chats[chat_id].flagUser(args[0])
+        if state == True:
+            bot.sendMessage(chat_id, "User %s Readied"   % args[0].first_name)
+        else:
+            bot.sendMessage(chat_id, "User %s Unreadied" % args[0].first_name)
+
+    #the name for this command is really up for grabs
+    def give_problem(self, chat_id, args):
+        newProb = self.chats[chat_id].dispense()  #returns filepath
+        if newProb is None:
+            bot.sendMessage(chat_id, "All problems in %s complete!" % self.chats[chat_id].cur_source)
+        
+        bot.sendPhoto(newProb, open(newProb, 'rb'))
+
+    def unready(self, chat_id, args):
+        self.chats[chat_id]
+
+    def select_source(self, chat_id, args):
+        try:
+            self.chats[chat_id].selectSource(args[1])
+            bot.sendMessage(chat_id, "Source Selected!")
+        except:
+            bot.sendMessage(chat_id, "No Such Source!")
 
 
+    def list_sources(self, chat_id, args):
+
+
+    def start(self, chat_id, args):
+        self.chats[chat_id] = ProblemManager
+        
+    def help(self, chat_id, args):
+        helpstr = "I have no idea, I'm so tired"
+        return helpstr
+
+
+
+PB = ProblemBot()
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    fromusr = msg['from']  #is a dict of 'first_name' : <users first name>, 'id' : <users id>
+
+    print("got command: {}".format(command))
+
+    split_msg = command.split()
+    method    = split_msg[0][1:]
+    args      = split_msg[1:]
+    args.insert(0,fromusr)
+
+    try:
+        getattr(PB, method)(chat_id, args)
+    except:
+        bot.sendMessage(chat_id, "Invalid Command")
+
+
+
+
+f = open('config.yml')
+cfgstr = f.read()
+mykey = yaml.load(cfgstr)['key']
+f.close()
+#initialize the bot as a global that everything relies on
+bot = telepot.Bot(mykey)
+
+print("i'm listening yo")
+bot.message_loop(handle, run_forever=True)
